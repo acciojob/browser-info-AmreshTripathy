1bac235f1c12dcc7ba7d3ce9658001a37903aece
Amresh Tripathy <amreshtripathy184@gmail.com>
2023-09-01 20:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+window.onload = function() {
+	document.getElementById("browser-info").innerHTML =  "You are using " + navigator.appName + " version " + navigator.appVersion;
+}
